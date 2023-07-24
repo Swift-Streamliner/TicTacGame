27c0d0313b5d747b9c828f5d6e869ceece3a9e00
tiger <thisisstreamliner@gmail.com>
2023-07-24 20:36:37 +0300

    Create Home View

--- TicTacGameSwiftUIMVVMFirebase/ContentView.swift
+++ TicTacGameSwiftUIMVVMFirebase/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @ViewBuilder
+    private func titleView() -> some View {
+        VStack(spacing: 20) {
+            Image(systemName: "number")
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 180, height: 180)
+            
+            Text("Tic Tac Toe")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
         }
-        .padding()
+        .foregroundColor(.indigo)
+        .padding(.top, 50)
+    }
+    
+    @ViewBuilder
+    private func buttonView() -> some View {
+        VStack (spacing: 15) {
+            Button {
+                
+            } label : {
+                Text("VS CPU")
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func main() -> some View {
+        VStack {
+            titleView()
+            Spacer()
+            buttonView()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.init(red: 0.91, green: 0.89, blue: 0.90))
+    }
+    
+    var body: some View {
+        main()
     }
 }
 
