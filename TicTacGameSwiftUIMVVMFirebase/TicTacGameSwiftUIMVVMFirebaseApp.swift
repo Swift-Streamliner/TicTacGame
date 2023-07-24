04640cf75ef9ee705617d33c9090d3b1d91d6260
tiger <thisisstreamliner@gmail.com>
2023-07-24 20:22:52 +0300

    Initial Commit

---
+++ TicTacGameSwiftUIMVVMFirebase/TicTacGameSwiftUIMVVMFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacGameSwiftUIMVVMFirebaseApp.swift
+//  TicTacGameSwiftUIMVVMFirebase
+//
+//  Created by MEI on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacGameSwiftUIMVVMFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
